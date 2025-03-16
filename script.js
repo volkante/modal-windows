fa32da44cdfcf63825d0ca4e4619e0851a0af660
volkantemelli <volkan.temelli@gmail.com>
2025-03-16 19:07:45 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,45 @@
+// Select elements and assign them to variables
+const firstButton = document.querySelector(".first-modal-button");
+const secondButton = document.querySelector(".second-modal-button");
+const thirdButton = document.querySelector(".third-modal-button");
+
+const closeButton = document.querySelector(".close-button");
+
+const article = document.querySelector("article");
+const articleContent = document.querySelector("article p");
+
+const contents = ["First content", "Second content", "Third content"];
+
+// Define functions
+const makeVisible = (element) => {
+  element.style.visibility = "visible";
+};
+
+const makeInvisible = (element) => {
+  element.style.visibility = "hidden";
+};
+
+const displayContent = (element, content) => {
+  element.textContent = content;
+};
+
+
+//Add event listeners
+firstButton.addEventListener("click", () => {
+  makeVisible(article);
+  displayContent(articleContent, "First content");
+});
+
+secondButton.addEventListener("click", () => {
+  makeVisible(article);
+  displayContent(articleContent, "Second content");
+});
+
+thirdButton.addEventListener("click", () => {
+  makeVisible(article);
+  displayContent(articleContent, "Third content");
+});
+
+closeButton.addEventListener("click", () => {
+  makeInvisible(article);
+})
